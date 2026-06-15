8653a30b78659a5987b89a45d37abdc41fd21016
xuetongyao <xuetongyao2001@gmail.com>
2026-06-15 14:26:10 +0800

    docs: update compressed graph mode inference log
    Signed-off-by: 0moyi0-2024 <1261320835@qq.com>

--- vllm_ascend/compilation/acl_graph.py
+++ vllm_ascend/compilation/acl_graph.py
@@ -20,6 +20,8 @@ from vllm.forward_context import BatchDescriptor, get_forward_context
 from vllm.logger import logger
 from vllm.platforms import current_platform
 
+from vllm_ascend.ascend_forward_context import _EXTRA_CTX
+
 from ..utils import weak_ref_tensors
 
 _STREAM_RESOURCE_ERROR_CODE = "207008"
